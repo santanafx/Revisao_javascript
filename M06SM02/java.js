810dbe2f370c45eebf0ccecddb8fddfcb2a5110e
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-01-18 14:51:48 -0300

    Metodos e propriedades

--- M06SM02/java.js
+++ M06SM02/java.js
@@ -14,3 +14,27 @@ console.log(`Tipo de ${num2} ${typeof num2}`);
 
 const num3 = parseFloat("21", 10);
 console.log(`Tipo de ${num3} ${typeof num3}`);
+
+//--------------------------------------------------
+
+const str = "Lucas Santana";
+
+//utilizando métodos
+
+console.log(`${str.toLowerCase()}`); //transformar letras maiusculas em minusculas
+console.log(`${str.toUpperCase()}`); //transformar letras minusculas em maiusculas
+console.log(`${str.charAt(3)}`); //retorna char da posicao 3
+console.log(`${str[3]}`); //o string tambem aceita a notacao do colchetes
+console.log(`${str.replace("a", "R")}`); //trocar letra
+console.log(`${str.replace(/a/g, "R")}`); //trocar letra com uso expressao regular
+console.log(`${str.length}`); //tamanho da variável,lenth é uma propriedade isso pode ser visto no autocomplete do vscode, a caixinha azul são de propriedades e a caixinha rosa são de métodos
+
+console.log(`${str.indexOf("a")}`); //a primeira ocorrencia do string é na posicao 3
+
+console.log(`${str.lastIndexOf("a")}`); //ultima ocorrencia da string
+
+console.log(`${str.substring(2, 5)}`); //imprime a posicao da string 2 até 5
+
+const str10 = "Lucas Santana    "; // string com espaços em branco no final
+
+console.log(`${str10.trim()}`); //remover espaços em brancos no final da string
