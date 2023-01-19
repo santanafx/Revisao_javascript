9d01dc9c95c690cb3c0cc47729e2e159983d18bf
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-01-19 09:27:54 -0300

    Fim revisao manipulacao DOM

---
+++ M06SM06/java.js
@@ -0,0 +1,22 @@
+//document.querySelector
+//.innerHTML
+//document.querySelectorAll
+//document.createElement
+//.appendChild
+//x.classList = devolve uma lista de card do elemento x
+//x.classList.add = adicionar uma classe
+//x.classList.remove = remover uma classe
+//x.classList.toggle = se a classe existe ele remove, se a classe nao existe ele adiciona
+
+//--------------------------------------------
+
+//dataset
+//ir no arquivo HTML e adicionar data-"nome que quiser"
+//para acessar o data pode-se usar document.querySelector("[data-exemplo]")
+
+//---------------------------------------------
+
+//.addEventListener('evento',funcao)
+//.target
+//Eventos: keyup,change,click
+//.preventDefault(); normalmente utilizando em botoes que enviam formulario
